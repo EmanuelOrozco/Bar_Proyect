2bd4580b09bdfd600b04a7e0e0bde5a089b61222
EmanuelOrozco <emanuelorozco2011@hotmail.com>
2024-11-22 05:12:53 -0500

    update models (Inventory, Order)

--- src/models/Inventory.py
+++ src/models/Inventory.py
@@ -1,29 +1,27 @@
 from models.Product import Product
 from typing import Dict
 
+
 class Inventory:
     inventory: Dict[Product, int]
 
     def __init__(self):
         self.inventory = {}
 
-    def update_quantity_product(self, product: Product, quantity: int, check: bool)->None:
-        if product not in self.inventory:
-            self.inventory[product] = 0
+    def update_quantity_product(self, name_product: str, quantity: int, check: bool) -> bool:
+        for product in self.inventory:
+            if product.name == name_product.lower():
+                if check:
+                    self.inventory[product] += quantity
+                else:
+                    if self.inventory[product] - quantity < 0 :
+                        print("Not enough product in the inventory")
+                        return product
+                    else:
+                        self.inventory[product] -= quantity
+                        return True
 
-        if check: 
-            self.inventory[product] += quantity
-        else:
-            if self.inventory[product] - quantity < 0 :
-                print("there are not enough products in inventory")
-            else:
-                self.inventory[product]-= quantity
-
-    def add_product(self, name_product: str, quantity: int) -> None:
-        for product in self.inventory.keys():
-            if product == name_product:
-                self.inventory[product] += quantity
-                return
-
-        new_product = Product(name = name_product)
+    def add_product(self, name_product: str, price: float, quantity: int) -> None:
+        new_product = Product(name_product, price)
         self.inventory[new_product] = quantity
+        return
--- src/models/Order.py
+++ src/models/Order.py
@@ -1,27 +1,37 @@
+from Inventory import Inventory
 from models.Product import Product
 from typing import Dict
+
 
 class Order:
     id_general: int = 0
     def __init__(self):
-        self.order_products: dict[Product, int] = {}
+        self.order_products: dict[Product:int] = {}
         self.total_order: float = 0.0
         self.value_tip: float = 0.0
         self.status = False
 
         self.id_order: int = self.id_general
-        self.id_general+=1
+        self.id_general += 1
 
+    def add_product(self, name: str , quantity: int, inventory: Inventory) -> None:
+        result =  inventory.update_quantity_product(name, quantity, False)
+        if result:
+            if result not in self.order_products:
+                self.order_products[result] = quantity
+            else:
+                self.order_products[result] += quantity
 
-    def add_product(self,name: str , quantity: int) ->None:
-        ...
+    def calculate_total(self) -> None:
+        count = 0
+        for product, quantity in self.order_products.items():
+            count += product.price * quantity
+        self.total_order = count
+        return
 
-    def calculate_total(self) ->None:
-        ...
-
-    def check_tip(self,tip:bool):
+    def check_tip(self, tip: bool):
         if tip:
-            self.value_tip = 0.10
+            self.value_tip = self.total_order * 0.10
             return
         else:
             return
