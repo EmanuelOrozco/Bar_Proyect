1ce740c381f218a4f8dbdae63a690150513277f5
EmanuelOrozco <emanuelorozco2011@hotmail.com>
2024-11-22 11:43:58 -0500

    update controller BarManager

--- src/controllers/BarManager.py
+++ src/controllers/BarManager.py
@@ -1,3 +1,7 @@
+from models.Waiter import Waiter
+from models.Table import Table
+
+
 class BarManager:
 
     def show_tables(self):
@@ -6,20 +10,20 @@ class BarManager:
     def show_waiter(self):
         ...
 
-    def show_total_effectiveness_per_waiter(self, id_waiter: int):
-        ...
+    def show_total_effectiveness_per_waiter(self, waiter: Waiter):
+        return waiter.waiter_effectiveness
+    
+    def show_total_customer_per_Waiter(self, waiter: Waiter):
+        return waiter.total_customers
 
-    def show_total_customer_per_Waiter(self, id_waiter: int):
-        ...
+    def show_total_tip_per_waiter(self, waiter: Waiter):
+        return waiter.total_value_tips
 
-    def show_total_tip_per_waiter(self, id_waiter: int):
-        ...
+    def update_waiter(self, waiter: Waiter):
+        waiter.update_waiter()
 
-    def update_waiter(self):
-        ...
+    def generate_order(self, table_id: int, waiter: Waiter):
+        waiter.generate_order(table_id)
 
-    def generate_order(self):
-        ...
-
-    def finish_order(self):
-        ...
+    def finish_order(self,table: Table, waiter: Waiter):
+        waiter.finish_order(table)
